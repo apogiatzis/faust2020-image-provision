e69dff4854c8fa37a000392bab89a5a163c28428
apogiatzis <apoyiatzis@outlook.com>
2020-07-12 05:48:32 +0100

    fix: gpg

--- scripts/get_image.sh
+++ scripts/get_image.sh
@@ -2,4 +2,5 @@
 
 wget https://faust.cs.fau.de/files/faustctf/2020/vulnbox.qcow2.gpg
 export GPG_TTY=$(tty)
-echo "Im g0nna hav3 t0 sc13nc3 th3 sh1t 0utta th1s" | gpg --passphrase-fd 0 --decrypt-files vulnbox.qcow2.gpg
+ls
+echo "Im g0nna hav3 t0 sc13nc3 th3 sh1t 0utta th1s" | gpg --batch --passphrase-fd 0 --decrypt-files vulnbox.qcow2.gpg
