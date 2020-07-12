a274f9451987b0c5fcaabdd6776c29cd04f606ff
apogiatzis <apoyiatzis@outlook.com>
2020-07-12 05:51:50 +0100

    fix: pkr fix

--- scripts/get_image.sh
+++ scripts/get_image.sh
@@ -2,5 +2,4 @@
 
 wget https://faust.cs.fau.de/files/faustctf/2020/vulnbox.qcow2.gpg
 export GPG_TTY=$(tty)
-ls
 echo "Im g0nna hav3 t0 sc13nc3 th3 sh1t 0utta th1s" | gpg --batch --passphrase-fd 0 --decrypt-files vulnbox.qcow2.gpg
