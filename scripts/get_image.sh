8cd00fd078dd34629fbfb020c0b75657c357ab0e
apogiatzis <apoyiatzis@outlook.com>
2020-07-12 05:40:58 +0100

    fix: gpg auto pass

--- scripts/get_image.sh
+++ scripts/get_image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 wget https://faust.cs.fau.de/files/faustctf/2020/vulnbox.qcow2.gpg
-echo "Im g0nna hav3 t0 sc13nc3 th3 sh1t 0utta th1s" | gpg --decrypt-files vulnbox.qcow2.gpg
+echo "Im g0nna hav3 t0 sc13nc3 th3 sh1t 0utta th1s" | gpg --passphrase-fd 0 --decrypt-files vulnbox.qcow2.gpg
